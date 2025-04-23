4695d0b7e9584915a2fc9be03611968b66fe4017
Aidan Avellanet <adavellanet@gmail.com>
2025-04-23 19:02:59 -0400

    Fourth Commit

---
+++ Structure 2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (){
+    int sentence[100];
+    int word[21]; //adds actra character for nill terninator
+        printf("C Programming is ________ \n");
+        scanf("%s", word);
+
+        printf("C Programming is %s\n", word);
+
+return 0;
+
+}
